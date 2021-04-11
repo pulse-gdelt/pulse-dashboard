13fbc518783714c990bc4647c2062332d592c506
rjchow <me@rjchow.com>
2021-04-11 15:38:48 +0800

    feat: added fetch

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,37 +1,55 @@
 import React, { useEffect, useState } from "react";
 import { ScatterPlot, data } from "../../components/ScatterPlot";
 
-
 // export function scatterPlotMouseOverHandler(event: any, datapoint: any): void {
 //   console.log(datapoint)
 // }
 
-export function CenterPane({scatterPlotMouseOverHandler}: {scatterPlotMouseOverHandler: any}): JSX.Element {
-  const [ ss, setSs ] = useState({}) 
+export function CenterPane({ scatterPlotMouseOverHandler }: { scatterPlotMouseOverHandler: any }): JSX.Element {
+  const [ss, setSs] = useState({} as any);
   useEffect(() => {
     const f = async () => {
-      console.log("sending query...")
-      const res = await fetch("https://localhost:9200/index3/_search", {
+      console.log("sending query...");
+      console.time("data-fetch");
+      const res = await fetch("https://walter-insurance-peter-generators.trycloudflare.com/index3/_search", {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify({
+          size: 10000,
           query: {
-            match: { Location: 'Singapore' }
-          }
-        })
-      })
-      setSs(res)
-    }
+            bool: {
+              must: {
+                range: {
+                  DateTime: {
+                    gte: "2021-03-12T00:00:00.000Z",
+                    lte: "2021-03-13T00:00:00.000Z",
+                  },
+                },
+              },
+            },
+          },
+        }),
+      } as any);
+      console.timeEnd("data-fetch");
+      setSs(await res.json());
+    };
     if (ss) {
-      console.log(ss)
+      console.log(ss);
     }
-    f()
-  }, [ss])
+    f();
+  }, []);
+
+  useEffect(() => {
+    console.log("newdata", ss);
+  }, [ss]);
 
   return (
     <>
       <div className="my-1 -mx-1 h-full bg-white shadow rounded-lg">
         <div className="px-4 py-5 h-full w-full flex">
-          <ScatterPlot dataParam={data} mouseoverHandler={scatterPlotMouseOverHandler} />
+          {ss.hits ? <ScatterPlot dataParam={ss.hits.hits} mouseoverHandler={scatterPlotMouseOverHandler} /> : null}
         </div>
       </div>
     </>
@@ -39,8 +57,8 @@ export function CenterPane({scatterPlotMouseOverHandler}: {scatterPlotMouseOverH
 }
 
 export function Dashboard(): JSX.Element {
-  const [ mouseoverDataPoint, scatterPlotMouseOverHandler ] = useState({})
-  
+  const [mouseoverDataPoint, scatterPlotMouseOverHandler] = useState({});
+
   return (
     <>
       <div className="flex flex-col my-1 px-1 w-1/5">
@@ -66,17 +84,14 @@ export function Dashboard(): JSX.Element {
     </>
   );
 }
-export function Articles({data}: {data: any}): JSX.Element {
-  
-  console.log("article data", data)
-  
+export function Articles({ data }: { data: any }): JSX.Element {
+  console.log("article data", data);
+
   return (
     <div className="m-1 h-full shadow rounded-lg bg-white px-4 py-5 border-b border-gray-200 sm:px-6 overflow-hidden">
       <h3 className="text-lg leading-6 font-medium text-gray-900">News Event Information</h3>
       <ul className="divide-y divide-gray-200">
-        <p>
-          {JSON.stringify(data.datapoint, null, 2)}
-        </p>
+        <p>{JSON.stringify(data.datapoint, null, 2)}</p>
         {/* <li className="relative bg-white py-5 px-4 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
           <div className="flex justify-between space-x-3">
             <div className="min-w-0 flex-1">
