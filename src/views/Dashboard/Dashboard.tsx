7de79014887a98adb4cbca30aaf46dceb501eeee
rjchow <me@rjchow.com>
2021-04-11 21:05:25 +0800

    feat: add mouseover prompt

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -144,7 +144,7 @@ export function Articles({ data }: { data: any }): JSX.Element {
     <div className="m-1 h-full shadow rounded-lg bg-white px-4 py-5 border-b border-gray-200 sm:px-6 overflow-hidden">
       <h3 className="text-lg leading-6 font-medium text-gray-900 mb-6">News Event Information</h3>
       <ul>
-        {pane}
+        {pane ?? <p className="text-3xl text-gray-400 italic">Mouseover something!</p>}
       </ul>
     </div>
   );
